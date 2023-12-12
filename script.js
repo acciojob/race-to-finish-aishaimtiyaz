54b784d680c15e415d7150a7a479eb8553542601
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-12 21:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,32 +2,41 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-// Function to create a promise that resolves after a random time
-function createRandomPromise() {
-  const randomTime = Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(`Promise resolved after ${randomTime} seconds`);
-    }, randomTime * 1000);
-  });
-}
 
-// Array of five promises
-const promises = [
-  createRandomPromise(),
-  createRandomPromise(),
-  createRandomPromise(),
-  createRandomPromise(),
-  createRandomPromise(),
-];
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Promise1");
+    }, Math.floor(Math.random() * 5000) + 1000);
+});
 
-// Use Promise.any to wait for the first promise to resolve
-Promise.any(promises)
-  .then(result => {
-    // Print the result to the output div
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = result;
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Promise2");
+    }, Math.floor(Math.random() * 5000) + 1000);
+});
+
+let promise3 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Promise3");
+    }, Math.floor(Math.random() * 5000) + 1000);
+});
+
+let promise4 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Promise4");
+    }, Math.floor(Math.random() * 5000) + 1000);
+});
+
+let promise5 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("Promise5");
+    }, Math.floor(Math.random() * 5000) + 1000);
+});
+
+const myArray = [promise1, promise2, promise3, promise4, promise5];
+
+Promise.any(myArray).then((result) => {
+    document.getElementById("output").innerText = result;
+});
+
+
